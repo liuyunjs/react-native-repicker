9c921f247aca719b3d45f6107b9527cd33a1771b
liuyunjs <429695034@qq.com>
2022-05-29 22:09:25 +0800

    默认props添加

--- library/main.ts
+++ library/main.ts
@@ -25,9 +25,13 @@ PickerView.defaultProps = {
   dark_itemColor: '#e5e5e5',
   dark_indicatorColor: '#c5c5c5',
   dark_overlayColor: '#2b2b2b',
+  indicatorColor: '#666',
+  itemColor: '#333',
+  overlayColor: '#fff',
 };
 
 Picker.defaultProps = {
   ...PickerView.defaultProps,
   dark_tintColor: '#1161c1',
+  tintColor: '#1073ea',
 };
